1d9887d7d82d840d5c3a739e911d3a1a0fc8e232
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2022-02-14 23:08:20 -0300

    Adicionando transiçao nas imagens do telefone

---
+++ src/image-transition.js
@@ -0,0 +1,50 @@
+(function startImageTransition() {
+  const images = document.getElementsByClassName("js-image-slide");
+
+  for (let i = 0; i < images.length; ++i) {
+      images[i].style.opacity = 1;
+  }
+
+  let top = 1;
+  let cur = images.length - 1;
+
+  setInterval(changeImage, 3000);
+
+  async function changeImage() {
+
+    let nextImage = (1 + cur) % images.length;
+
+    images[cur].style.zIndex = top + 1;
+    images[nextImage].style.zIndex = top;
+
+    await transition();
+
+    images[cur].style.zIndex = top;
+    images[nextImage].style.zIndex = top + 1;
+
+    top = top + 1;
+
+    images[cur].style.opacity = 1;
+
+    cur = nextImage;
+  }
+
+  function transition() {
+    return new Promise(function (resolve, reject) {
+
+        let del = 0.01;
+        let id = setInterval(changeOpacity, 10);
+
+        function changeOpacity() {
+          images[cur].style.opacity -= del;
+          if (images[cur].style.opacity <= 0) {
+              clearInterval(id);
+              resolve();
+          }
+        }
+
+    });
+
+  }
+
+})();
